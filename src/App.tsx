9a9a00c28baf824d8f3c576ba206d262b99633ff
yjz <624661385@qq.com>
2022-06-05 20:30:53 +0800

    feat: 增加页面渲染崩溃的ErroBoundary

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import { AuthenticatedApp } from 'authenticated-app';
+import { ErrorBoundary } from 'components/error-boundary';
+import { FullPageErrorFallback } from 'components/lib';
 import { useAuth } from 'context/auth-context';
 import { UnauthenticatedApp } from 'unauthenticated-app';
 import './App.css';
 
 function App() {
   const { user } = useAuth();
-  return <div className="App">{user ? <AuthenticatedApp /> : <UnauthenticatedApp />}</div>;
+  return (
+    <div className="App">
+      <ErrorBoundary fallbackRender={FullPageErrorFallback}>
+        {user ? <AuthenticatedApp /> : <UnauthenticatedApp />}
+      </ErrorBoundary>
+    </div>
+  );
 }
 
 export default App;
